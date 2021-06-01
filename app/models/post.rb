b960ba3f8ec4e9a49f47f027a9ff1628e919f44c
Gabriela Solìs <solisch.gabriela@gmail.com>
2021-06-01 01:03:58 -0600

    I change the root, made validations of the posts and for users in the controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,19 @@
 class Post < ApplicationRecord
   belongs_to :user
+  #Active storage from rails 5
+  has_one_attached :image
+  has_one_attached :photo
+  #has_many_attached :... --> por si quiero varias imagenes, en este caso solo quiero dos 
+
+  #Action text from rails 6
+  has_rich_text :body
+
+  #Validations on this body:
+  validates :title, length: (minimun: 5)
+  validates :body, length: (minimun: 25)
+
+  #for the size of the img(i can do with css but also in this way)
+  def authomize_img(image, x, y)
+    return image.variant(resize_to_fill: [x,y]).processed  
+  end
 end
