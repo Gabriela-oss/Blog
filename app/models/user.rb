a59a4ad548c0c5127679324d7e61311edd904067
Gabriela Solìs <solisch.gabriela@gmail.com>
2021-05-31 23:21:37 -0600

    I edited the routes, also add a navbar with many interesting details, and did the seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    #gaby@example.com -> self.email.split('@') -> ['gaby', 'example.com'] -> [0] -> 'gaby'.capitalize -> Gaby  
+  def username 
+    return email.split("@")[0].capitalize
+  end
 end
