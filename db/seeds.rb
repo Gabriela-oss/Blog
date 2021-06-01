da5bde9ed6b2d8adc0a9a320d3930787336bfcdb
Gabriela Solìs <solisch.gabriela@gmail.com>
2021-06-01 17:15:21 -0600

    Made posts in the seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,14 @@ User.create!(
   password_confirmation:'123123',
   admin: true
 )
+
+25.times do |i|
+  post = Post.new
+  post.title = Faker::Lorem.sentence(word_count: 3, random_words_to_add: 7)
+  post.body = Faker::Lorem.paragraph_by_chars(number: 1500)
+  post.user = User.first
+  post.image.attach(io: open("https://picsum.photos/1920/1080"), filename: "#{i}_image.jpg")
+  post.photo.attach(io: open("https://picsum.photos/1920/1080"), filename: "#{i}_photo.jpg")
+  post.views = Faker::Number.between(from: 1, to: 5000)
+  post.save
+end
